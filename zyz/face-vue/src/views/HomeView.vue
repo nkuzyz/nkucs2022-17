d84f1087d2de728c29a87c2b66ef83cba160892d
张哈哈 <93150961+nkuzyz@users.noreply.github.com>
2022-08-16 15:46:33 +0800

    zyz

---
+++ zyz/face-vue/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+ <div class="home">
+   <el-button type="primary">我是按钮</el-button>
+ </div>
+</template>
+
+<!--<script>-->
+<!--// @ is an alias to /src-->
+
+<!--export default {-->
+<!--  name: "Home_view",-->
+<!--};-->
+<!--</script>
+
