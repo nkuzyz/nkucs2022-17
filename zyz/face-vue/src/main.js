d49dd828f91b4aeb6c7efb75dec0488266ba7647
张哈哈 <93150961+nkuzyz@users.noreply.github.com>
2022-08-16 17:25:19 +0800

    zyz

--- zyz/face-vue/src/main.js
+++ zyz/face-vue/src/main.js
@@ -5,3 +5,18 @@ import store from "./store";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
+export default {
+  data() {
+    return {
+      disabled: false,
+    };
+  },
+  methods: {
+    warnDisabled() {
+      this.disabled = true;
+      setTimeout(() => {
+        this.disabled = false;
+      }, 1500);
+    },
+  },
+};
