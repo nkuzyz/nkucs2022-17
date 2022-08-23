b4cac28b8180ef141b44056ed6650b88c94a8a94
AkShuuichi <wumailc@foxmail.com>
2022-08-23 20:02:53 +0800

    0823(前端施工完成)

---
+++ wyf/0823mask/front-end/src/components/MyFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 固定footer-->
+  <div class="footer" :class="{ 'fixed-footer': fixed }">
+    <span class="word">© 2022</span>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="less" scoped>
+.fixed-footer {
+    left:0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+}
+.footer {
+  background-color: @xTxColor;
+  height: 35px;
+  margin-bottom: 0;
+  text-align: center;
+}
+.word{
+    color:#fff;
+    font-size: 14px;
+    text-align: center;
+}
+</style>
